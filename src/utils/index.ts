ac3a30dd6201442657bb3ea03bfc01d2210a2f87
duythai2108 <thaiduy21082000@gmail.com>
2023-08-19 15:00:52 +0700

    typescript basic

---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+// export function total(a, b) {
+//   return a + b;
+// }
+export function total(a: number, b: number) {
+  return a + b;
+}
+total(5, 7);
+//  const variable ; dataType = value
+// const age: number = 30;
+// const name: string = 'thaind';
+// const isStudent: boolean = false;
